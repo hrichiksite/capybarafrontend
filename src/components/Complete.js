b7b1ad2438eb2d0ff97c499d18d5473afbc56f62
Hrichik Mazumder <57860916+hrichiksite@users.noreply.github.com>
2024-07-29 15:35:29 +0530

    Update Complete.js

--- src/components/Complete.js
+++ src/components/Complete.js
@@ -14,7 +14,7 @@ export default function Complete() {
   const getAnswer = async () => {
     setLoading(true)
     const response = await fetch(
-      `https://laughing-capybara-7xv7j4vgg52rgpj-3000.app.github.dev/whatif`,
+      `https://codedaywhatifbackend.hrichik.xyz/whatif`,
       {
         method: 'POST',
         headers: {
@@ -35,7 +35,7 @@ export default function Complete() {
 
 return (
        <>
-    <section className="w-full h-dvh py-12 md:py-24 lg:py-32 xl:py-48 bg-black">
+    <section className="w-full min-h-screen py-12 md:py-24 lg:py-32 xl:py-48 bg-black">
     <div className="container px-4 md:px-6">
       <div className="grid gap-6 items-center">
         <div className="flex flex-col justify-center space-y-4 text-center">
