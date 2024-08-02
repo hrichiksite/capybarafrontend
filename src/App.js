16162b92e892264ba0aea3bec179420f4259b879
Hrichik Mazumder <57860916+hrichiksite@users.noreply.github.com>
2024-08-02 16:22:16 +0530

    chore: Add react-hot-toast for displaying loading messages. Add loading messages.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
 import Complete from './components/Complete';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <>
+    <div><Toaster/></div>
     <Complete/>
     </>
     );
